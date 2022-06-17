a350a113c96227f8e5bab0d213f1bf65cb20ba37
Gabriel Gore <gabriel59kg@gmail.com>
2022-06-16 21:14:55 -0400

    pyproject and invoke tasks setups

---
+++ tasks.py
@@ -0,0 +1,34 @@
+import invoke
+
+
+@invoke.task
+def sort(ctx, path=".", check=False):
+    """Sort module imports."""
+    print("  sorting imports ...")
+    args = ["isort", path, "--profile", "black"]
+    if check:
+        args.append("--check-only")
+    ctx.run(" ".join(args))
+
+
+@invoke.task
+def fmt(ctx, path=".", sort_=True, check=False):
+    """Run code formatter."""
+    print("  formatting ...")
+
+    args = ["black", path]
+    if check:
+        args.append("--check")
+    ctx.run(" ".join(args))
+    if sort_:
+        sort(ctx, path, check)
+
+
+@invoke.task
+def lint(ctx, path="grit"):
+    ctx.run(f"pylint {path}")
+
+
+@invoke.task
+def types(ctx, path="."):
+    ctx.run(f"mypy {path}")
